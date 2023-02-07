1d589266b8a387e3fff5471b3dcf9b4bb64fac4a
jhw4319 <jhw4319@naver.com>
2023-02-07 18:11:25 +0900

    Initial commit

---
+++ src/main/java/jhwspring/helloboot/UnderstandingBootApplication.java
@@ -0,0 +1,13 @@
+package jhwspring.helloboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UnderstandingBootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UnderstandingBootApplication.class, args);
+    }
+
+}
---
+++ src/test/java/jhwspring/helloboot/UnderstandingBootApplicationTests.java
@@ -0,0 +1,13 @@
+package jhwspring.helloboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UnderstandingBootApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
